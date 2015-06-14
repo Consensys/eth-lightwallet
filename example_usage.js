1e1e2a4aa5b45a7ec1262660fd3996c1a0f59cd2
Christian Lundkvist <christian.lundkvist@gmail.com>
2015-06-14 14:03:21 -0400

    Add seed to example usage. Remove redundant dependencies.

--- example_usage.js
+++ example_usage.js
@@ -5,23 +5,39 @@
 var web3 = require('web3')
 var lw = require('ethlightjs')
 var ks = lw.keystore
+var Transaction = require('ethereumjs-tx')
 
 web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 
 var source = '\ncontract NameCoin {\n\n    struct Item {\n\taddress owner;\n\tuint value;\n    }\n\n    mapping (uint => Item) registry;\n\n    function register(uint key) {\n\tif (registry[key].owner == 0) {\n\t    registry[key].owner = msg.sender;\n\t}\n    }\n\n    function transferOwnership(uint key, address newOwner) {\n\tif (registry[key].owner == msg.sender) {\n\t    registry[key].owner = newOwner;\n\t}\n    }\n\n    function setValue(uint key, uint newValue) {\n\tif (registry[key].owner == msg.sender) {\n\t    registry[key].value = newValue;\n\t}\n    }\n\n    function getValue(uint key) constant returns (uint value) {\n\treturn registry[key].value;\n    }\n\n    function getOwner(uint key) constant returns (address owner) {\n\treturn registry[key].owner;\n    }\n}\n'
 
-var privkey = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
-ks.addPrivateKey(privkey, 'mypassword')
+//var privkey = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+//ks.addPrivateKey(privkey, 'mypassword')
+
+var seed = 'unhappy nerve cancel reject october fix vital pulse cash behind curious bicycle'
+ks.setSeed(seed, 'mypassword')
+
 var sendingAddr = ks.getAddresses()[0]
 console.log(sendingAddr)
 var nonce = web3.eth.getTransactionCount('0x' + sendingAddr)
 console.log('Nonce: ' + nonce)
 var code = web3.eth.compile.solidity(source).slice(2)
 
+// Send a value transaction
+var valueTx = new Transaction({gasLimit: 300000,
+			       gasPrice: 1000000000000,
+			       to : 'eba8cdda5058cd20acbe5d1af35a71cfc442450e',
+			       value : 1000000000000000000,
+			       nonce: nonce})
+
+var signedValueTx = ks.signTx(valueTx.serialize.toString('hex'), 'mypassword', sendingAddr)
+console.log('Value TX: ' + signedValueTx)
+
+
 txOptions = {
     gasPrice: 10000000000000,
     gasLimit: 3000000,
-    value: 0,
+    value: 10000000,
     nonce: nonce,
     data: code
 }
@@ -54,7 +70,9 @@ var signedSetValueTx = ks.signTx(setValueTx, 'mypassword', sendingAddr)
 console.log('SetValueTx: ' + signedSetValueTx)
 
 // Check that the owner is sendingAddr
-var myContract = web3.eth.contract(abi).at('0x' + contractData.addr)
+var contractAddr = '6c19d6af83d0d335a006ae424a426ef4d139d827'
+// var contractAddr = contractData.addr
+var myContract = web3.eth.contract(abi).at('0x' + contractAddr)
 var owner = myContract.getOwner(123)
 console.log('Owner: ' + owner)
 
--- index.js
+++ index.js
@@ -2,6 +2,5 @@ var ethlightjs = {};
 
 ethlightjs = require('./lib/lightwalletutils.js');
 ethlightjs.keystore = require('./lib/keystore.js');
-ethlightjs.hdkeygen = require('./lib/hdkeygen.js');
 
 module.exports = ethlightjs;
