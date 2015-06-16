9287c15ff5f11a59e35c33cbe37df2f3081cc349
Zach Ferland <zachferland@Zacharys-MBP.home>
2015-06-15 20:45:36 -0400

    add some test coverage to keystore.js, add relevant set of test data

--- test/keystore.js
+++ test/keystore.js
@@ -1,31 +1,61 @@
 var expect = require('chai').expect
 var keyStore = require('../lib/keystore')
-
+var fixtures = require('./fixtures/keystore')
 
 describe("Keystore", function() {
 
-  describe("_encryptSeed", function() {
+  // Can't directly test the encrypt/decrypt functions
+  // since salt and iv is used.
+  describe("_encryptSeed _decryptSeed", function() {
+
+    fixtures.valid.forEach(function (f) {
+      it('encrypts the seed then returns same seed decrypted ' + '"' + f.mnSeed + '"', function () {
+
+        var encryptedSeed = keyStore._encryptSeed(f.mnSeed, f.password)
+        var decryptedSeed = keyStore._decryptSeed(encryptedSeed, f.password)
+
+        expect(decryptedSeed).to.equal(f.mnSeed)
+      })
+    })
 
   });
 
-  describe("_decryptSeed", function() {
+  describe("_encryptKey _decryptKey", function() {
 
-  });
+    fixtures.valid.forEach(function (f) {
+      it('encrypts the key then returns same key decrypted ' + '"' + f.pubKeyHex + '"', function () {
 
-  describe("_encryptKey", function() {
+        var encryptedKey = keyStore._encryptKey(f.pubKeyHex, f.password)
+        var decryptedKey = keyStore._decryptKey(encryptedKey, f.password)
 
-  });
-
-  describe("_decryptKey", function() {
+        expect(decryptedKey).to.equal(f.pubKeyHex)
+      })
+    })
 
   });
 
   describe("_computeAddressFromPrivKey", function() {
 
+    fixtures.valid.forEach(function (f) {
+      it('generates valid address from private key ' + '"' + f.HDPrivKey + '"', function () {
+
+        var address = keyStore._computeAddressFromPrivKey(f.HDPrivKey)
+
+        expect(address).to.equal(f.address)
+      })
+    })
+
   });
 
 
   describe("setSeed", function() {
+    it('throws when a seed has already been set', function() {
+      expect(function(){
+        keyStore.setSeed(fixtures.valid[0].mnSeed, fixtures.valid[0].password);
+        keyStore.setSeed(fixtures.valid[1].mnSeed, fixtures.valid[1].password);
+
+      }).to.throw(/Seed has already been set/)
+    })
 
   });
 
